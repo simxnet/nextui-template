75ce6b7ea7770faa4d3aae4df760e78eb1bc1e7c
simxnet <simxnet@envs.net>
2024-03-28 14:03:25 +0100

    feat: github

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,41 @@
 import type { Config } from "tailwindcss";
+import { commonColors, nextui } from "@nextui-org/react";
+
+const primaryColor = commonColors.cyan; // easy to change ;)
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
-};
+  darkMode: "class",
+  plugins: [nextui({
+    addCommonColors: true,
+    prefix: "app", // change if want to
+    defaultExtendTheme: "dark",
+    defaultTheme: "dark",
+    themes: {
+      main: {
+        layout: {
+          radius: {
+            small: "0.25rem",
+            medium: "0.5rem",
+            large: "0.75rem",
+          }
+        },
+        colors: {
+          background: "#05040a",
+          primary: {
+            ...primaryColor,
+            foreground: commonColors.black,
+            DEFAULT: primaryColor["500"]
+          }
+        }
+      }
+    }
+  })]
+} satisfies Config
+
 export default config;
