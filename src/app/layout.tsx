22eec3004cadf633e2eb7dda3627b32cf27ac51a
simxnet <simxnet@envs.net>
2024-03-28 14:09:54 +0100

    feat: biome

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
+import { Providers } from "@/components/common/providers";
 import type { Metadata } from "next";
 import { Rubik as FontSans } from "next/font/google";
-import { Providers } from "@/components/common/providers";
 
 import "@/styles/globals.css";
 import { cn } from "@nextui-org/react";
@@ -8,22 +8,20 @@ import { cn } from "@nextui-org/react";
 const font = FontSans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Next template",
-  description: "Custom next.js template",
+	title: "Next template",
+	description: "Custom next.js template",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className="main">
-      <body className={cn("p-5", font.className)}>
-        <Providers>
-          {children}
-        </Providers>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className="main">
+			<body className={cn("p-5", font.className)}>
+				<Providers>{children}</Providers>
+			</body>
+		</html>
+	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
-  return (
-    <>
-      <h1 className="text-4xl font-bold">Hello world</h1>
-    </>
-  )
+	return (
+		<>
+			<h1 className="text-4xl font-bold">Hello world</h1>
+		</>
+	);
 }
